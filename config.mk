2a5c76c53ee3e56540ddae25faea589ea30f881e
Abdul Dakkak <abduld@wolfram.com>
2018-02-21 16:07:51 -0600

    merge

--- config.mk
+++ config.mk
@@ -225,10 +225,6 @@ USE_PROFILER=1
 # whether compile with options for MXNet developer
 DEV = 0
 
-# whether compile with debug
-<<<<<<< HEAD
-DEBUG = 1
-
 DEBUG = 0
 USE_GLOG=1
 USE_OPERATOR_TUNING = 0
