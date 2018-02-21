0dc2b8ace6d05c0643a684ad52ba80f8b215b07f
Abdul Dakkak <abduld@wolfram.com>
2018-02-21 17:04:23 -0600

    add prof script

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+MXNET_GPU_WORKER_NTHREADS=1 MXNET_ENGINE_TYPE=NaiveEngine nvprof -f --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
+MXNET_GPU_WORKER_NTHREADS=1 MXNET_ENGINE_TYPE=NaiveEngine nvprof -f --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
 MXNET_GPU_WORKER_NTHREADS=1 MXNET_ENGINE_TYPE=NaiveEngine nvprof -f --export-profile `hostname`_profile_2.timeline.nvprof ./image-classification-predict
