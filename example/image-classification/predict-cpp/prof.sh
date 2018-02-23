a8b86e99dc171e7805288715d73bb897dc53d6ba
Abdul Dakkak <abduld@wolfram.com>
2018-02-22 19:25:11 -0600

    openmp is not used

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-export GLOG_v=4
+export OMP_NUM_THREADS=1
+# export MXNET_ENGINE_TYPE=NaiveEngine
+
+# export GLOG_v=4
 export UPR_CLIENT=1
 export MXNET_CPU_PRIORITY_NTHREADS=1
-export MXNET_GPU_WORKER_NTHREADS=4
+export MXNET_GPU_WORKER_NTHREADS=1
+# nvprof -f --track-memory-allocations on --print-api-trace --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
-nvprof -f --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
+nvprof -f --print-gpu-trace --print-api-trace --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_2.timeline.nvprof ./image-classification-predict
