d2c896c3601976a98eb8e8ad5794eeab84e63f1b
Abdul Dakkak <abduld@wolfram.com>
2018-02-23 03:48:45 -0600

    updates

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -11,5 +11,6 @@ export MXNET_CPU_PRIORITY_NTHREADS=1
 export MXNET_GPU_WORKER_NTHREADS=1
 # nvprof -f --track-memory-allocations on --print-api-trace --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
-nvprof -f --print-gpu-trace --print-api-trace --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
+nvprof -f --cpu-profiling on --print-gpu-trace --print-api-trace --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
+nvprof -f --cpu-profiling on --analysis-metrics --export-profile `hostname`_profile_1.analysis.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_2.timeline.nvprof ./image-classification-predict
