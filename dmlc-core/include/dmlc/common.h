6f46c9745b7f863fba69a079d0e34b1e3558eab7
Abdul Dakkak <abduld@wolfram.com>
2018-02-22 21:29:00 -0600

    work

--- dmlc-core/include/dmlc/common.h
+++ dmlc-core/include/dmlc/common.h
@@ -10,6 +10,27 @@
 #include <string>
 #include <sstream>
 
+#include "nvToolsExt.h"
+
+#ifndef PUSH_RANGE
+#define PUSH_RANGE0(name,cid,FUNCTION_NAME) { \
+    int color_id = cid; \
+static const uint32_t colors[] = { 0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff, 0x0000ffff, 0x00ff0000, 0x00ffffff };\
+static const int num_colors = sizeof(colors)/sizeof(uint32_t); \
+    color_id = color_id%num_colors;\
+    nvtxEventAttributes_t eventAttrib = {0}; \
+    eventAttrib.version = NVTX_VERSION; \
+    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
+    eventAttrib.colorType = NVTX_COLOR_ARGB; \
+    eventAttrib.color = colors[color_id]; \
+    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
+    eventAttrib.message.ascii = name " in " #  FUNCTION_NAME; \
+    nvtxRangePushEx(&eventAttrib); \
+}
+#define PUSH_RANGE(name,cid) PUSH_RANGE0(name, cid,FUNCTION_NAME)
+#define POP_RANGE() nvtxRangePop();
+#endif
+
 namespace dmlc {
 /*!
  * \brief Split a string by delimiter
