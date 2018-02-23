34a429abe2a9db65a611eb37d39a870a5733bd3a
Abdul Dakkak <abduld@wolfram.com>
2018-02-22 19:35:07 -0600

    update prof

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+export OMP_NUM_THREADS=1
 export GLOG_v=4
 export UPR_CLIENT=1
 export MXNET_CPU_PRIORITY_NTHREADS=1
-export MXNET_GPU_WORKER_NTHREADS=4
+export MXNET_GPU_WORKER_NTHREADS=1
 nvprof -f --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_1.timeline.nvprof ./image-classification-predict
 nvprof -f --export-profile `hostname`_profile_2.timeline.nvprof ./image-classification-predict
