ee983bb8b86309add3979b81609380748bea5c54
Abdul Dakkak <abduld@wolfram.com>
2018-02-26 12:51:28 -0600

    add python example no opencv on ppc64le

--- example/image-classification/predict-python/image-classification-predict.py
+++ example/image-classification/predict-python/image-classification-predict.py
@@ -6,13 +6,12 @@ path='http://data.mxnet.io/models/imagenet-11k/'
 
 sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-152', 0)
 mod = mx.mod.Module(symbol=sym, context=mx.cpu(), label_names=None)
-mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))], 
+mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))],
          label_shapes=mod._label_shapes)
 mod.set_params(arg_params, aux_params, allow_missing=True)
 with open('synset.txt', 'r') as f:
     labels = [l.rstrip() for l in f]
 
-%matplotlib inline
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
