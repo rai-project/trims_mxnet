ebda7e67e6678df40b9ee1e93af83e5500482abd
Abdul Dakkak <abduld@wolfram.com>
2018-03-01 01:08:46 -0600

    work

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -9,6 +9,48 @@ export OMP_NUM_THREADS=1
 export MXNET_ENGINE_TYPE=NaiveEngine
 export MXNET_GPU_WORKER_NTHREADS=2
 
+
+export UPR_MODEL_NAME=alexnet
+# first time
+./image-classification-predict `hostname`_0
+
+# second time
+./image-classification-predict `hostname`_1
+
+# third time
+./image-classification-predict `hostname`_2
+
+export UPR_MODEL_NAME=vgg16
+# first time
+./image-classification-predict `hostname`_0
+
+# second time
+./image-classification-predict `hostname`_1
+
+# third time
+./image-classification-predict `hostname`_2
+
+export UPR_MODEL_NAME=resnet-152-11k
+# first time
+./image-classification-predict `hostname`_0
+
+# second time
+./image-classification-predict `hostname`_1
+
+# third time
+./image-classification-predict `hostname`_2
+
+export UPR_MODEL_NAME=squeezenetv1
+# first time
+./image-classification-predict `hostname`_0
+
+# second time
+./image-classification-predict `hostname`_1
+
+# third time
+./image-classification-predict `hostname`_2
+
+export UPR_MODEL_NAME=squeezenetv1.1
 # first time
 ./image-classification-predict `hostname`_0
 
