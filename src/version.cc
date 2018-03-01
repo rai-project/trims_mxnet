306b4b642890046410d1714a8d288b2c1c197ed4
Cheng Li <cli99@illinois.edu>
2018-03-01 00:53:53 -0500

    add version

---
+++ src/version.cc
@@ -0,0 +1,4 @@
+/* version.c */
+#include "./version.h"
+const char * build_git_sha = "ac5bffc90f0034df9e091a7b3aa12d150df26a0e";
+const char * build_git_time = "Thu Dec  3 18:03:58 EST 2009";
---
+++ src/version.h
@@ -0,0 +1,9 @@
+/*version.h*/
+#ifndef VERSION_H_
+#define VERSION_H_
+
+extern const char * build_git_time;
+extern const char * build_git_sha;
+
+
+#endif /* VERSION_H_ */
