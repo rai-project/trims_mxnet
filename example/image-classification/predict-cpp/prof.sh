4468a31ec0aec3fe006adcae69e1a8afff5e005a
Cheng Li <cli99@illinois.edu>
2018-03-05 00:09:29 -0500

    update run sh

--- example/image-classification/predict-cpp/prof.sh
+++ example/image-classification/predict-cpp/prof.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 DATE=`date '+%Y-%m-%d-%H-%M-%S'`
 export UPR_CLIENT=1
@@ -9,56 +9,24 @@ export OMP_NUM_THREADS=1
 export MXNET_ENGINE_TYPE=NaiveEngine
 export MXNET_GPU_WORKER_NTHREADS=2
 
+declare -a models=(
+    bvlc_alexnet_1.0
+    bvlc_googlenet_1.0
+)
 
-export UPR_MODEL_NAME=alexnet
-# first time
-./image-classification-predict `hostname`_0
+for i in "${models[@]}"
+do
+    echo start to run model = $i
+    export UPR_MODEL_NAME=$i
+    # first time
+    ./image-classification-predict `hostname`_0
 
-# second time
-./image-classification-predict `hostname`_1
+    # second time
+    ./image-classification-predict `hostname`_1
 
-# third time
-./image-classification-predict `hostname`_2
-
-export UPR_MODEL_NAME=vgg16
-# first time
-./image-classification-predict `hostname`_0
-
-# second time
-./image-classification-predict `hostname`_1
-
-# third time
-./image-classification-predict `hostname`_2
-
-export UPR_MODEL_NAME=resnet-152-11k
-# first time
-./image-classification-predict `hostname`_0
-
-# second time
-./image-classification-predict `hostname`_1
-
-# third time
-./image-classification-predict `hostname`_2
-
-export UPR_MODEL_NAME=squeezenetv1
-# first time
-./image-classification-predict `hostname`_0
-
-# second time
-./image-classification-predict `hostname`_1
-
-# third time
-./image-classification-predict `hostname`_2
-
-export UPR_MODEL_NAME=squeezenetv1.1
-# first time
-./image-classification-predict `hostname`_0
-
-# second time
-./image-classification-predict `hostname`_1
-
-# third time
-./image-classification-predict `hostname`_2
+    # third time
+    ./image-classification-predict `hostname`_2
+done
 
 exit
 # nvprof -f --track-memory-allocations on --print-api-trace --export-profile `hostname`_profile_0.timeline.nvprof ./image-classification-predict
