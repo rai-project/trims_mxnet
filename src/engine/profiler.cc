6881e8279e84c7cfaf5c8274103aa1b0bf5af55b
Abdul Dakkak <abduld@wolfram.com>
2018-03-06 05:36:49 -0600

    updates

--- src/engine/profiler.cc
+++ src/engine/profiler.cc
@@ -166,7 +166,7 @@ namespace engine {
       pid = 0;
       tid = 0;
     }
-    json j = {{"name", name}, {"cat", category}, {"ph", begin_end}, {"ts", ts}, {"pid", pid}, {"tid", tid}, {"args", args}};
+    json j = {{"name", name}, {"cat", category}, {"ph", begin_end}, {"ts", ts}, {"pid", pid}, {"tid", tid}, {"args", args}, {"begin", opr_stat->opr_start_rel_micros}, {"end", opr_stat->opr_end_rel_micros}};
     return j;
   }
 
