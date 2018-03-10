c294033948a7cb555a81c0e337faf8f21a73ae6d
Abdul Dakkak <abduld@wolfram.com>
2018-03-10 10:23:21 -0600

    add prop

--- src/c_api/ipc.h
+++ src/c_api/ipc.h
@@ -71,7 +71,7 @@ namespace upr {
 using namespace mxnet;
 
 static const auto HOME         = dmlc::GetEnv("HOME", std::string("/home/abduld"));
-static const auto UPR_ENABLED  = dmlc::GetEnv("UPR_ENABLED", false);
+static const auto UPR_ENABLED  = dmlc::GetEnv("UPR_ENABLED", true);
 static const auto is_client    = dmlc::GetEnv("UPR_CLIENT", false);
 static const auto UPR_BASE_DIR = dmlc::GetEnv("UPR_BASE_DIR", HOME + std::string("/carml/data/mxnet/"));
 
